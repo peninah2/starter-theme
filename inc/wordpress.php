402f8aa5ac0d2f7d2d33305fadbd7d881569f49c
peninah2 <peninah@highlightcreative.com>
2020-08-25 12:00:25 -0400

    Update wordpress.php
    Add disable auto-update email notification

--- inc/wordpress.php
+++ inc/wordpress.php
@@ -5,6 +5,13 @@
  * @package HCStarter
 **/
 
+
+//Disable plugin auto-update email notification
+add_filter('auto_plugin_update_send_email', '__return_false');
+
+//Disable theme auto-update email notification
+add_filter('auto_theme_update_send_email', '__return_false');
+
  /**
   * Dont Update the Theme
   *
