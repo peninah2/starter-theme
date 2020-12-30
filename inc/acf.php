ef5cd76a31419e9987ea01b315b6aa761a0d0f08
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:28:17 -0500

    Update acf.php

--- inc/acf.php
+++ inc/acf.php
@@ -12,14 +12,17 @@
 if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
-		'page_title' 	=> 'THEME NAME Global Content',
-		'menu_title'	=> 'THEME Settings',
-		'menu_slug' 	=> 'theme-settings',
+		'page_title' 	=> 'Spam-protected contact information',
+		'menu_title'	=> 'Spam-protected contact',
+		'menu_slug' 	=> 'contact-settings',
 		'capability'	=> 'edit_posts',
+		'icon_url'	 	=> 'dashicons-email',
+		'position' 		=> 20,
 		'redirect'		=> false
 	));
 	
 }
+
 
 /* Contact anti-spambot
  * To use in template: 
@@ -27,24 +30,27 @@ if( function_exists('acf_add_options_page') ) {
  * $phone = antispambot( get_field( 'encoded_phone' ) );
 ------------------------------------------*/
 
-//add_shortcode( 'encoded_email', 'hct_encoded_email_shortcode' );
+
+add_shortcode( 'encoded_email', 'hct_encoded_email_shortcode' );
 function hct_encoded_email_shortcode() {
 	
-	$email = antispambot( get_field( 'encoded_email', 'option' ) );
+	$email = antispambot( get_field( 'email', 'option' ) );
 	$emailLink = '<a href="mailto:' . $email . '">' . $email . '</a>';
 	
 	return $emailLink;
 
 }
 
-//add_shortcode( 'encoded_phone', 'hct_encoded_phone_shortcode' );
+add_shortcode( 'encoded_phone', 'hct_encoded_phone_shortcode' );
 function hct_encoded_phone_shortcode() {
 	
-	$phone = antispambot( get_field( 'encoded_phone', 'option' ) );
+	$phone = antispambot( get_field( 'phone', 'option' ) );
 	$phoneLink = '<a href="tel:' . $phone . '">' . $phone . '</a>';
 	
 	return $phoneLink;
 
 }
+
+
 
 
