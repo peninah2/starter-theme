7353feda903fe4d869486a60bac733e20259a401
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:29:38 -0500

    Update navigation.php

--- inc/navigation.php
+++ inc/navigation.php
@@ -6,15 +6,7 @@
 
 **/ 
  
-
-// Custom header
-function hct_header() {
-	?>
-			<a href="/" class="logo"><h1>Site title</h1></a>
-	<?php
-	}
-//add_action('genesis_header', 'hct_header');
- 
+ add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu', 'secondary' => 'Secondary Navigation Menu' ) );
  
 
  // Repositions primary navigation menu to header
@@ -37,3 +29,41 @@ function hct_secondary_menu_args( $args ) {
 	return $args;
 
 }
+
+
+// Adding custom menus
+// add_action( 'after_setup_theme', 'hct_register_nav_menu', 0 );
+function hct_register_nav_menu(){
+	register_nav_menus( array(
+		'footer_menu_1'  => __( 'Footer 1', 'text_domain' ),
+		'footer_menu_2'  => __( 'Footer 2', 'text_domain' ),
+		'mobile_menu'  => __( 'Responsive', 'text_domain' ),
+	) );
+}
+
+// If using separate menu for responsive menu
+// add_action('genesis_header', 'hct_responsive_menu_button', 11 );
+function hct_responsive_menu_button() {
+	
+	echo '<a href="#" class="menu-toggle"><span class="toggle-icon"></span></a>'; 
+	
+	wp_nav_menu( array( 
+		'theme_location' => 'mobile_menu', 
+		'container' => 'nav', 
+		'container_class' => 'mobile-menu-wrap flexbox align-items-center justify-content-center', 
+		'menu_class' => 'genesis-nav-menu mobile-menu',
+		) 
+	);
+	
+}
+
+
+// Scroll to top 
+function hct_scroll_top() {
+	?>
+	<a href="#" class="button scroll-top" id="scroll-top">
+ 		<i class="arrow up"></i>
+	</a>
+	<?php
+}
+add_action( 'genesis_after_content', 'hct_scroll_top' );
