b05314808373c884e6f26e99b181c3ce5a42456d
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:28:59 -0500

    Update genesis-changes.php

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -10,8 +10,6 @@
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
 add_theme_support( 'genesis-responsive-viewport' );
 add_theme_support( 'genesis-structural-wraps', array( 'header', 'menu-secondary', 'site-inner', 'footer-widgets', 'footer' ) );
-add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu', 'secondary' => 'Secondary Navigation Menu' ) );
-add_theme_support( 'genesis-footer-widgets', 3 );
 
 // Adds support for accessibility.
 add_theme_support(
