2c456627a27d1026433c3bd6e4c06470a7cae85c
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:29:56 -0500

    Update presets.php

--- inc/presets.php
+++ inc/presets.php
@@ -10,43 +10,69 @@
 add_theme_support( 'disable-custom-font-sizes' );
 
 // -- Editor Font Styles
+// -- Editor Font Styles
 add_theme_support( 'editor-font-sizes', array(
-array(
-	'name'      => __( 'Small', 'theme-name' ),
-	'shortName' => __( 'S', 'theme-name' ),
-	'size'      => 14,
-	'slug'      => 'small'
-),
-array(
-	'name'      => __( 'Normal', 'theme-name' ),
-	'shortName' => __( 'M', 'theme-name' ),
-	'size'      => 20,
-	'slug'      => 'normal'
-),
-array(
-	'name'      => __( 'Large', 'theme-name' ),
-	'shortName' => __( 'L', 'theme-name' ),
-	'size'      => 24,
-	'slug'      => 'large'
-),
+	array(
+		'name'      => __( 'Small', 'theme-name' ),
+		'shortName' => __( 'S', 'theme-name' ),
+		'size'      => 14,
+		'slug'      => 'small'
+	),
+
+	array(
+		'name'      => __( 'Medium', 'theme-name' ),
+		'shortName' => __( 'M', 'theme-name' ),
+		'size'      => 20,
+		'slug'      => 'medium'
+	),
+
+	array(
+		'name'      => __( 'Medium-Large', 'theme-name' ),
+		'shortName' => __( 'ML', 'theme-name' ),
+		'size'      => 22,
+		'slug'      => 'med-large'
+	),		
+	
+	array(
+		'name'      => __( 'Large', 'theme-name' ),
+		'shortName' => __( 'L', 'theme-name' ),
+		'size'      => 24,
+		'slug'      => 'large'
+	),
+	
+	array(
+		'name'      => __( 'Extra Large', 'theme-name' ),
+		'shortName' => __( 'XL', 'theme-name' ),
+		'size'      => 30,
+		'slug'      => 'xlarge'
+	),	
+	
 ) );
 
 // -- Disable Custom Colors
-add_theme_support( 'disable-custom-colors' );
+// add_theme_support( 'disable-custom-colors' );
 
 // -- Editor Color Palette
 add_theme_support( 'editor-color-palette', array(
-array(
-	'name'  => __( 'Primary', 'theme-name' ),
-	'slug'  => 'primary',
-	'color' => '#03a9f4',
-),
 
-array(
-	'name'  => __( 'White', 'theme-name' ),
-	'slug'  => 'white',
-	'color' => '#ffffff',
-),
+	array(
+		'name'  => __( 'White', 'theme-name' ),
+		'slug'  => 'white',
+		'color' => '#ffffff',
+	),
+	
+	array(
+		'name'  => __( 'Grey', 'theme-name' ),
+		'slug'  => 'grey',
+		'color' => '#ddd',
+	),
+
+	array(
+		'name'  => __( 'Black', 'theme-name' ),
+		'slug'  => 'black',
+		'color' => '#111',
+	),
+	
 ) );
 
 
