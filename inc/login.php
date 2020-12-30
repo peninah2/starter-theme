b91d7a7bc1af4839b79bb733710150a394d46b32
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:29:23 -0500

    Update login.php

--- inc/login.php
+++ inc/login.php
@@ -22,24 +22,28 @@ add_filter( 'login_headertext', '__return_empty_string' );
  */
 function hct_login_logo() {
 
-	$logo_path = '/images/logo.png';
+	$logo_path = '/images/logo/logo.svg';
 	if( ! file_exists( get_stylesheet_directory() . $logo_path ) )
 		return;
 
 	$logo = get_stylesheet_directory_uri() . $logo_path;
     ?>
     <style type="text/css">
-    .login h1 a {
-        background-image: url(<?php echo $logo;?>);
-        background-size: contain;
-        background-repeat: no-repeat;
-		background-position: center center;
-        display: block;
-        overflow: hidden;
-        text-indent: -9999em;
-        width: 312px;
-        height: 100px;
-    }
+		body {
+			background: #FFFCF5;
+		}
+		
+		.login h1 a {
+			background-image: url(<?php echo $logo;?>);
+			background-size: contain;
+			background-repeat: no-repeat;
+			background-position: center center;
+			display: block;
+			overflow: hidden;
+			text-indent: -9999em;
+			width: 312px;
+			height: 100px;
+		}
     </style>
     <?php
 }
