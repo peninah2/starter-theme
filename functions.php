fce830ffd127dafec7c109669e8d15c9288b42f2
peninah2 <peninah@highlightcreative.com>
2020-12-30 12:25:29 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ function hct_enqueue_scripts_styles() {
 	);
 
 	wp_enqueue_style(
-		'theme-name-fonts',
+		'themename-fonts',
 		'//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,700',
 		array(),
 		CHILD_THEME_VERSION
@@ -40,7 +40,7 @@ function hct_enqueue_scripts_styles() {
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 	wp_enqueue_script(
-		'theme-name-responsive-menu',
+		'themename-responsive-menu',
 		get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js",
 		array( 'jquery' ),
 		CHILD_THEME_VERSION,
@@ -48,14 +48,14 @@ function hct_enqueue_scripts_styles() {
 	);
 	
 	wp_localize_script(
-		'theme-name-responsive-menu',
+		'themename-responsive-menu',
 		'genesis_responsive_menu',
 		hct_responsive_menu_settings()
 	);
 
 	wp_enqueue_script(
-		'theme-name',
-		get_stylesheet_directory_uri() . '/js/theme-name.js',
+		'themename',
+		get_stylesheet_directory_uri() . '/js/themename.js',
 		array( 'jquery' ),
 		CHILD_THEME_VERSION,
 		true
@@ -79,25 +79,23 @@ if ( ! isset( $content_width ) ) {
 	$content_width = 702; // Pixels.
 }
 
-// General cleanup
-include_once( get_stylesheet_directory() . '/inc/wordpress.php' );
-include_once( get_stylesheet_directory() . '/inc/genesis-changes.php' );
-
 // Theme
 include_once( get_stylesheet_directory() . '/inc/presets.php' );
 include_once( get_stylesheet_directory() . '/inc/layouts.php' );
-include_once( get_stylesheet_directory() . '/inc/navigation.php' ); //includes header
+include_once( get_stylesheet_directory() . '/inc/navigation.php' );
+include_once( get_stylesheet_directory() . '/inc/header.php' );
 include_once( get_stylesheet_directory() . '/inc/footer.php' );
 include_once( get_stylesheet_directory() . '/blocks/acf-block-setup.php' );
 
 // Dashboard
-include_once( get_stylesheet_directory() . '/inc/disable-editor.php' );
 include_once( get_stylesheet_directory() . '/inc/login.php' );
 include_once( get_stylesheet_directory() . '/inc/dashboard.php' );
 
 // Other
 include_once( get_stylesheet_directory() . '/inc/gravity-forms.php' );
 include_once( get_stylesheet_directory() . '/inc/acf.php' ); // non-blocks
+include_once( get_stylesheet_directory() . '/inc/misc.php' );
+include_once( get_stylesheet_directory() . '/inc/genesis-changes.php' );
 
 
 // Image Sizes
@@ -142,9 +140,9 @@ function sp_post_info_filter($post_info) {
 function hct_responsive_menu_settings() {
 
 	$settings = array(
-		'mainMenu'         => __( 'Menu', 'theme-name' ),
+		'mainMenu'         => __( 'Menu', 'themename' ),
 		'menuIconClass'    => 'dashicons-before dashicons-menu',
-		'subMenu'          => __( 'Submenu', 'theme-name' ),
+		'subMenu'          => __( 'Submenu', 'themename' ),
 		'subMenuIconClass' => 'dashicons-before dashicons-arrow-down-alt2',
 		'menuClasses'      => array(
 			'combine' => array(
