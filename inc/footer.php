2d3f50b02f7d6aaa960c1187a9d5967d18a8a50f
peninah2 <peninah@highlightcreative.com>
2021-10-19 10:02:13 -0400

    Update footer.php
    Added block support to footer

--- inc/footer.php
+++ inc/footer.php
@@ -14,15 +14,27 @@
 
 **/
 
-
-// add_theme_support( 'genesis-footer-widgets', 3 );
-
-
 // Remove Genesis footer
 remove_action('genesis_footer', 'genesis_do_footer');
+remove_action('genesis_footer', 'genesis_footer_markup_open', 5);
+remove_action('genesis_footer', 'genesis_footer_markup_close', 15);
 
 // Removes footer entry meta
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+
+// Activate block-based widgets
+add_filter( 'use_widgets_block_editor', '__return_true' );
+
+// Activate block/widget based area
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+
+// Shortcode: current year
+add_shortcode( 'current_year', 'hct_current_year' );
+function hct_current_year() {
+	$year = date('Y');
+	return $year;
+}
 
 
 // Custom footer
