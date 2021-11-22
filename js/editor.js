4067aa9bfc5200ebadda06bf6ac97cd1f292c8c9
peninah2 <peninah@highlightcreative.com>
2021-11-22 13:16:33 -0500

    updates

--- js/editor.js
+++ js/editor.js
@@ -1,30 +1,31 @@
 wp.domReady( () => {
 
+	// Remove spacer block as we have a custom one with more settings
+	wp.blocks.unregisterBlockType( 'core/spacer' );
 
-	// 	wp.blocks.unregisterBlockType( 'core/spacer' ); Remove spacer block in favor of custom
-
+	wp.blocks.registerBlockStyle( array('core/heading', 'core/paragraph'), [ 	
+		{
+			name: 'sans',
+			label: 'Sans',
+		},
+		{
+			name: 'serif',
+			label: 'Serif',
+			isDefault: true,
+		},	
+	
+	]);	
+	
 	wp.blocks.unregisterBlockStyle(
 		'core/button',
 		[ 'squared', 'fill' ]
 	);
-	
-	wp.blocks.registerBlockStyle( 'core/group', [ 
-		{
-			name: 'default',
-			label: 'Default',
-			isDefault: true,
-		},
-		{
-			name: 'flexbox',
-			label: 'Flexbox',
-		},	
-	
-	]);	
 
 	wp.blocks.unregisterBlockStyle(
 		'core/separator',
 		[ 'default', 'wide', 'dots' ],
 	);
+
 
 	wp.blocks.unregisterBlockStyle(
 		'core/quote',
