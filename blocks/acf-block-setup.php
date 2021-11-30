f93509eaf0519dc14cb225420528fa3cb02df8e7
peninah2 <peninah@highlightcreative.com>
2021-11-30 11:55:39 -0500

    Remove slider/swipers

--- blocks/acf-block-setup.php
+++ blocks/acf-block-setup.php
@@ -41,7 +41,7 @@ function hct_register_custom_blocks() {
 }
 add_action('acf/init', 'hct_register_custom_blocks' );
 
-
+add_filter( 'block_categories_all', 'hct_custom_block_category', 10, 2);
 function hct_custom_block_category( $categories, $post ) {
 	return array_merge(
 		$categories,
@@ -53,7 +53,6 @@ function hct_custom_block_category( $categories, $post ) {
 		)
 	);
 }
-add_filter( 'block_categories_all', 'hct_custom_block_category', 10, 2);
 
 /*
  * Only load Featherlight scripts if team member block is present
@@ -74,112 +73,8 @@ function hct_teamblock_featherlight_script() {
 // add_action( 'wp_enqueue_scripts', 'hct_teamblock_featherlight_script' );
 
 
-/*
- * Only load Swiper scripts if slider block is present
- */
-
-function hct_slider_block_swiper_enqueue_script() {
-	
-	if ( has_block( 'acf/testimonial-slider' ) || has_block( 'acf/logo-slider' ) ) {
-		wp_enqueue_script(
-			'swiper-js',
-			'//unpkg.com/swiper/swiper-bundle.min.js',
-			array( 'jquery' ),
-			CHILD_THEME_VERSION,
-			true
-		);
-		
-		wp_enqueue_style(
-			'swiper-styles',
-			'//unpkg.com/swiper/swiper-bundle.min.css',
-			array(),
-		);
-	}
-}
-//add_action( 'wp_enqueue_scripts', 'hct_slider_block_swiper_enqueue_script' );
 
 
-
-// Now add specific for this slider 
-function hct_testimonial_slider() {
-	
-	if ( has_block( 'acf/testimonial-slider' ) || is_post_type_archive( 'featured' ) ) { 
-	?>
-	
-	<script type='text/javascript'>
-		jQuery(function ($) {
-		$(document).ready(function(){
-			
-			var swiper = new Swiper('.testimonial-slider', {
-				autoplay: {
-					delay: 10000,
-				  },
-				
-				speed: 1000,
-				loop: true, 
-				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-				  },
-			});		
-			
-		});	
-	});	
-	</script>
-	
-	<?php
-	}
-}
-add_action( 'wp_footer', 'hct_testimonial_slider', 50 );
-
-// Now add specific for this slider 
-function hct_logo_slider() {
-	
-	if ( has_block( 'acf/logo-slider' ) ) { 
-	?>
-	
-		<script type='text/javascript'>
-		jQuery(function ($) {
-		$(document).ready(function(){
-			
-			var swiper = new Swiper('.logo-slider', {
-				  
-				loop: true,
-				
-				slidesPerView: 1,
-				speed: 5000,
-				
-				breakpoints: {
-					600: {
-					  slidesPerView: 2,
-					  spaceBetween: 30,
-					},
-					
-					1280: {
-					  slidesPerView: 3,
-					  spaceBetween: 30,
-					  slidesPerGroup: 3,
-					}
-				},
-				  
-				  
-				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-				  },
-			});		
-			
-		});	
-	});	
-	</script>
-	
-	<?php
-	}
-}
-add_action( 'wp_footer', 'hct_logo_slider', 50 );
-
-
-// Custom spacer block 
 // Custom spacer block 
 if( function_exists('acf_add_local_field_group') ):
 
