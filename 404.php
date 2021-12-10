88fd2881ffc9e4a5f6b95f8152c9b0fbffdac9fe
peninah2 <peninah@highlightcreative.com>
2021-12-09 19:02:14 -0500

    Update 404

--- 404.php
+++ 404.php
@@ -11,14 +11,14 @@ function hct_genesis_404() {
 
 		echo  '<article class="entry"><div class="post hentry">';
 
-		echo '<div class="wp-block-group alignfull" style="text-align:center;"><div class="wp-block-group__inner-container">';
+		echo '<div class="wp-block-group" style="text-align:center;">';
 
 		printf( '<h1 class="entry-title">%s</h1>', __( 'That page isn\'t here.', 'genesis' ) );
 
 		echo '<p>Try checking the menu above, or, <a href="/contact">contact us.</a></p>';
 		
-		echo '</div></div>';
-		echo '</article></div>';
+		echo '</div>';
+		echo '</div></article>';
 
 }
 
