82f51b43640ae2535ade0d4208352a5acc14ac42
peninah2 <peninah@highlightcreative.com>
2023-05-09 10:01:26 -0400

    Updates

--- inc/acf.php
+++ inc/acf.php
@@ -14,7 +14,7 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
 		'page_title' 	=> 'Contact',
 		'menu_title'	=> 'Contact',
-		'menu_slug' 	=> 'Contact',
+		'menu_slug' 	=> 'contact',
 		'capability'	=> 'edit_posts',
 		'icon_url'	 	=> 'dashicons-phone',
 		'position' 		=> 20,
@@ -57,5 +57,92 @@ function hct_address_shortcode() {
 }
 
 
+			if( function_exists('acf_add_local_field_group') ):
 
+acf_add_local_field_group(array(
+	'key' => 'group_636d45739c9c5',
+	'title' => 'Contact',
+	'fields' => array(
+		array(
+			'key' => 'field_636d45728968a',
+			'label' => 'Phone',
+			'name' => 'phone',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
+			'key' => 'field_636d45888968b',
+			'label' => 'Email',
+			'name' => 'email',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
+			'key' => 'field_636d458e8968c',
+			'label' => 'Address',
+			'name' => 'address',
+			'aria-label' => '',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'rows' => 3,
+			'placeholder' => '',
+			'new_lines' => 'br',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'contact',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+));
+
+endif;		
 
