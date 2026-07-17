0818b6563a1d3f6ab5d115b86105ce8602163268
peninah2 <peninah@highlightcreative.com>
2026-07-17 10:50:20 -0400

    updated svg block

--- blocks/svg/render.php
+++ blocks/svg/render.php
@@ -1,21 +1,26 @@
-<?php 
+<?php
 /*
  * SVG image
- * Custom html blocks do the same thing, but they always show in html mode and we want to show preview mode. 
+ * Custom HTML blocks do the same thing, but they always show in HTML mode and we want to show preview mode.
  */
- 
- $svg = get_field( 'svg_code' );
- 
 
-/* Set class & alignment */
+$svg = get_field( 'svg_code' );
+
+/* Set classes & alignment */
 $classes = ['svg-img'];
-if( !empty( $block['className'] ) )
-    $classes = array_merge( $classes, explode( ' ', $block['className'] ) ); 
 
-if( !empty($block['align']) ) {
-    $classes[] = 'align' . $block['align'];
+if ( ! empty( $block['className'] ) ) {
+	$classes = array_merge( $classes, explode( ' ', $block['className'] ) );
 }
 
+if ( ! empty( $block['align'] ) ) {
+	$classes[] = 'align' . $block['align'];
+}
 
-echo '<div class="' . join( ' ', $classes ) . '">' . $svg . '</div>';
+/* Anchor / id */
+$anchor = '';
+if ( ! empty( $block['anchor'] ) ) {
+	$anchor = ' id="' . esc_attr( sanitize_title( $block['anchor'] ) ) . '"';
+}
 
+echo '<div class="' . esc_attr( join( ' ', $classes ) ) . '"' . $anchor . '>' . $svg . '</div>';
